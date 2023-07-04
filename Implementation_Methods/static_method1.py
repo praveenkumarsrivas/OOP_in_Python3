dbd3609cb305be5d1b6c8b506fa6c6f3421d3d8b
Praveen Kumar Srivas <Praveen.Srivas@radisys.com>
2023-07-04 11:22:48 +0530

    added files

--- Implementation_Methods/static_method1.py
+++ Implementation_Methods/static_method1.py
@@ -1,18 +1,9 @@
-class Dates:
-    def __init__(self,date):
-        self.date = date
-        
-    def getDate(self):
-        return self.date
+class Circle:
+    # Insert your code here
+    def __init__(self, radius):
+        self.radius= radius
+    def area(self):
+        return 3.14*(self.radius*self.radius)
     
-    @staticmethod #optional for static method
-    def change_format(date):
-        return date.replace("/","-")
-    
-    
-date = Dates("10-12-1995")
-print(date.getDate())
-
-db_date = "10/12/1995"
-dateWithDash = Dates.change_format(db_date)
-print(dateWithDash)
+c = Circle(11)
+print(c.area())
